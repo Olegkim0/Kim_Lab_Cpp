74b07e699001ca3610638c66918b0b393bb69def
Oleg <Olegkim0@gmail.com>
2021-10-03 14:41:40 +0300

    fix user input, StationEdit

--- Kim_Lab_cpp1/Source.cpp
+++ Kim_Lab_cpp1/Source.cpp
@@ -1,37 +1,51 @@
 #include <iostream>
+#include <limits>
+#include <string>
 
 
 struct Pipe {
-    int id; 
+    int id;
     int diameter;
     int length;
-    bool isWorking;  
+    bool isWorking;
 };
 
 Pipe AddPipe(int id)
 {
-    std::cout << "AddPipe\n";
+    std::cout << "Adding Pipe\n";
 
-
-    Pipe p{ 0 };
+    Pipe p;
 
     std::cout << "Input diameter:\n";
-    std::cin >> p.diameter;
-
-    std::cout << "Input length:\n";
-    std::cin >> p.length;
-
+    p.diameter = 0;
 
     while (p.diameter <= 0) {
-        std::cin >> p.diameter;
-        std::cout << "Wrong diameter, try again\n";
-        continue;
+        std::string str_diameter;
+        std::cin.ignore();
+        std::cin >> str_diameter;
+        if (str_diameter.find_first_not_of("0123456789") != std::string::npos) {
+            std::cout << "The input contains not only digits\n";
+            std::cout << "Try again: \n";
+        }
+        else {
+            p.diameter = std::stoi(str_diameter);
+        }
     }
 
+    std::cout << "Input length:\n";
+    p.length = 0;
+
     while (p.length <= 0) {
-        std::cin >> p.length;
-        std::cout << "Wrong length, try again\n";
-        continue;
+        std::string str_length;
+        std::cin.ignore();
+        std::cin >> str_length;
+        if (str_length.find_first_not_of("0123456789") != std::string::npos) {
+            std::cout << "The input contains not only digits\n";
+            std::cout << "Try again: \n";
+        }
+        else {
+            p.length = std::stoi(str_length);
+        }
     }
 
     return p;
@@ -54,28 +68,66 @@ struct Station {
 
 Station AddStation(int id) {
 
-    Station s{ 0 };
+    std::cout << "Adding Station\n";
+
+    Station s;
 
     std::cout << "Input name:\n";
     std::cin >> s.name;
-    while (s.name.length() <= 0) {
-        std::cin >> s.name;
-        std::cout << "Wrong name, try again";
-        continue;
-    }
 
     std::cout << "Input number of workshops:\n";
-    std::cin >> s.number_of_workshops;
+    s.number_of_workshops = 0;
+    while (s.number_of_workshops <= 0) {
+        std::string str_number_of_workshop;
+        std::cin.ignore();
+        std::cin >> str_number_of_workshop;
+        if (str_number_of_workshop == "0") {
+            std::cout << "Wrong input";
+        }
+        if (str_number_of_workshop.find_first_not_of("0123456789") != std::string::npos) {
+            std::cout << "The input contains not only digits\n";
+            std::cout << "Try again: \n";
+        }
+        else {
+            s.number_of_workshops = std::stoi(str_number_of_workshop);
+        }
+    }
 
     std::cout << "Input number of working workshops:\n";
-    std::cin >> s.number_of_working_workshops;
-    
-    std::cout << "Input Efficiency (0 <= e <= 1):\n";
-    std::cin >> s.Efficiency;
-    while (s.Efficiency <= 0 && s.Efficiency > 1) {
-        std::cin >> s.Efficiency;
-        std::cout << "Wrong Efficiency, try again";
-        continue;
+    s.number_of_working_workshops = -1;
+    while (s.number_of_working_workshops < 0 || s.number_of_working_workshops > s.number_of_workshops) {
+        std::string str_number_of_working_workshops;
+        std::cin.ignore();
+        std::cin >> str_number_of_working_workshops;
+
+        if (str_number_of_working_workshops.find_first_not_of("0123456789") != std::string::npos) {
+            std::cout << "Wrong input\n";
+            std::cout << "Try again: \n";
+            continue;
+        }
+        else {
+            s.number_of_working_workshops = std::stoi(str_number_of_working_workshops);
+        }
+
+        if (s.number_of_working_workshops > s.number_of_workshops) {
+            std::cout << "number of working workshops > number of workshops\n";
+        }
+    }
+
+
+    std::cout << "Input Efficiency (0 < e <= 100):\n";
+    s.Efficiency = 0;
+    while (s.Efficiency <= 0 || s.Efficiency > 100) {
+        std::string str_Efficiency;
+        std::cin.ignore();
+        std::cin >> str_Efficiency;
+        if (str_Efficiency.find_first_not_of("0123456789") != std::string::npos) {
+            std::cout << "The input contains not only digits\n";
+            std::cout << "Try again: \n";
+        }
+        else {
+            s.Efficiency = std::stoi(str_Efficiency);
+        }
     }
 
     return s;
@@ -83,22 +135,22 @@ Station AddStation(int id) {
 
 Station StationEdit(Station s) {
     std::cout << "Editing station\n";
-    std::cin >> s.Efficiency;
+    std::cin >> s.number_of_working_workshops;
     return s;
 }
-    
+
 
 void Output(Pipe& p)
 {
-    std::cout << "Output Pipe(s)\n";
-    std::cout << "Id: " << p.id;
+    std::cout << "\nOutput Pipe(s)";
+    std::cout << "\nId: " << p.id;
     std::cout << "\nDiameter: " << p.diameter;
     std::cout << "\nLength: " << p.length << "\n";
 }
 
 void Output(Station& s) {
-    std::cout << "Output station(s)\n";
-    std::cout << "Id: " << s.id;
+    std::cout << "\nOutput station(s)";
+    std::cout << "\nId: " << s.id;
     std::cout << "\nname: " << s.name;
     std::cout << "\nnumber of workshops: " << s.number_of_workshops;
     std::cout << "\nnumber of working workshops: " << s.number_of_working_workshops;
@@ -110,56 +162,57 @@ int main()
     int pipe_counter = 0;
     int station_counter = 0;
     int choose;
-
+    Station s1;
+    Pipe p1;
     bool menu = true;
 
     while (menu) {
 
         std::cout << "menu\n";
-        
-        Station s;
 
         int choose;
         std::cin >> choose;
 
-        switch(choose){
-            case 0:
-                std::cout << "\nExit\n";
-                menu = false;
-                break;
+        switch (choose) {
+        case 0:
+            std::cout << "\nExit\n";
+            menu = false;
+            break;
 
-            case 1:
-                Pipe p1 = AddPipe(pipe_counter);
-                break;
+        case 1:
+            p1 = AddPipe(pipe_counter);
+            break;
 
-            case 2:
-                //Station s1 = AddStation(station_counter);
-                break;
+        case 2:
+            s1 = AddStation(station_counter);
+            break;
 
-            case 3:
-                Output(p1);
-                //Output(s1);
-                break;
+        case 3:
+            // if (vector_of_Pipes.length != 0){}
+            Output(p1);
+            // if (vector_of_Station.length != 0){}
+            Output(s1);
+            break;
 
-            case 4:
-                PipeEdit(p1);
-                break;
+        case 4:
+            PipeEdit(p1);
+            break;
 
-            case 5:
-                //StationEdit(s1);
-                break;
+        case 5:
+            StationEdit(s1);
+            break;
 
-            case 6: 
-                break;
+        case 6:
+            //Save();
+            break;
 
-            case 7:
-                break;
+        case 7:
+            //Load();
+            break;
 
-            default:
-                break;
-            }
-
-        //break;
+        default:
+            break;
+        }
     }
     return 0;
 }
